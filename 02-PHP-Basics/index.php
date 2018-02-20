201b46076bc90a8305e77c01ffd0a63e2894f560
Edxael <zerphyr.r@gmail.com>
2018-02-19 21:31:39 -0700

    PHP Files

---
+++ 02-PHP-Basics/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP code page</title>
+  </head>
+  <body>
+    <h1>Runing PHP</h1>
+    <p>Here is the code..</p>
+    <br><br>
+
+    <?
+    $x = 5;
+
+    echo "The variable is: ", $x;
+    // echo $x;
+    ?>
+
+
+  </body>
+</html>
